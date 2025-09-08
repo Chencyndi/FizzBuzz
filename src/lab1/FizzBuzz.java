4457e1cec61e7a960576dd8c8afe42f46be7b8b0
cyndi <cyndi.chen@mail.utoronto.ca>
2025-09-08 13:59:27 -0400

    Task1&2

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,12 +7,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i++<100) {
-            doFizzBuzz(i);
+        while (i<100) {
+            i = doFizzBuzz(i);
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    private static int doFizzBuzz(int i) {
         // for (int i = 1; i < 100; i++) {
 
         // Find out which numbers divide i.
@@ -37,5 +37,7 @@ class FizzBuzz {
             System.out.println(i);
 
         }
+        i++;
+        return i;
     }
 }
